9e88ac3efd0e1a941be75463c519048b48c9bbf1
zCubed3 <liamr2023@gmail.com>
2022-11-02 23:06:41 -0700

    Update required functions for real

--- src/real.rs
+++ src/real.rs
@@ -9,10 +9,10 @@ use std::fmt::*;
 ///
 /// This trait is already implemented for [f32] and [f64]
 pub trait RealNumber:
-    Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Neg<Output=Self> + // Arithmetic
-    AddAssign + SubAssign + MulAssign + DivAssign + // Arithmetic with assign
-    PartialEq + PartialOrd + // Equality
-    Clone + Copy + Default + Display // Usability
+Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Div<Output=Self> + Neg<Output=Self> + // Arithmetic
+AddAssign + SubAssign + MulAssign + DivAssign + // Arithmetic with assign
+PartialEq + PartialOrd + // Equality
+Clone + Copy + Default + Display // Usability
     where Self: Sized {
     fn real_sqrt(&self) -> Self;
 
@@ -21,6 +21,14 @@ pub trait RealNumber:
     fn real_tan(&self) -> Self;
 
     fn real_abs(&self) -> Self;
+
+    fn real_min(&self, min: Self) -> Self;
+    fn real_max(&self, max: Self) -> Self;
+    fn real_clamp(&self, min: Self, max: Self) -> Self;
+
+    fn real_pow(&self, exp: Self) -> Self;
+
+    fn real_lerp(&self, to: Self, alpha: Self) -> Self;
 
     fn real_rad_to_deg() -> Self;
     fn real_deg_to_rad() -> Self;
@@ -39,7 +47,7 @@ impl RealNumber for f32 {
     fn real_sqrt(&self) -> Self {
         self.sqrt()
     }
-    
+
     fn real_sin(&self) -> Self {
         self.sin()
     }
@@ -54,6 +62,26 @@ impl RealNumber for f32 {
 
     fn real_abs(&self) -> Self {
         self.abs()
+    }
+
+    fn real_min(&self, min: Self) -> Self {
+        return self.min(min);
+    }
+
+    fn real_max(&self, max: Self) -> Self {
+        return self.max(max);
+    }
+
+    fn real_clamp(&self, min: Self, max: Self) -> Self {
+        return self.clamp(min, max);
+    }
+
+    fn real_pow(&self, exp: Self) -> Self {
+        return self.powf(exp);
+    }
+
+    fn real_lerp(&self, to: Self, alpha: Self) -> Self {
+        return (1.0 - alpha) * self + alpha * to;
     }
 
     fn real_rad_to_deg() -> Self {
@@ -95,6 +123,26 @@ impl RealNumber for f64 {
         self.abs()
     }
 
+    fn real_min(&self, min: Self) -> Self {
+        return self.min(min);
+    }
+
+    fn real_max(&self, max: Self) -> Self {
+        return self.max(max);
+    }
+
+    fn real_clamp(&self, min: Self, max: Self) -> Self {
+        return self.clamp(min, max);
+    }
+
+    fn real_pow(&self, exp: Self) -> Self {
+        return self.powf(exp);
+    }
+
+    fn real_lerp(&self, to: Self, alpha: Self) -> Self {
+        return (1.0 - alpha) * self + alpha * to;
+    }
+
     fn real_rad_to_deg() -> Self {
         57.2957795131f64
     }
@@ -112,8 +160,8 @@ impl RealNumber for f64 {
     }
 }
 
-#[cfg(feature="real_t_is_double")]
+#[cfg(feature = "real_t_is_double")]
 pub type Real = f64;
 
-#[cfg(not(feature="real_t_is_double"))]
+#[cfg(not(feature = "real_t_is_double"))]
 pub type Real = f32;
